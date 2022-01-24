86a06c899d985f30b08040cab7ef8f5b73936c8a
elijahb22 <elibusick11@gmail.com>
2022-01-23 21:51:25 -0500

    added new note function and delete note function

--- routes/apiRoutes/dbRoutes.js
+++ routes/apiRoutes/dbRoutes.js
@@ -1,15 +1,19 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
+const { displayNewNote, deleteNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
 });
-
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     const note = displayNewNote(req.body, notes);
     res.json(note)
+});
+router.delete('/notes/:id', (req, res) => {
+    const result = deleteNote(req.params.id, notes);
+    res.json(result);
 });
 
 module.exports = router; 
